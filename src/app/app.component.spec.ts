2926fa1d4898fa28716dbdce68b74c48a9310b9b
akp <akplearner@gmail.com>
2023-06-17 23:01:01 -0500

    Init correct folder format.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MyPortfolio'`, () => {
+  it(`should have as title 'AnthonyKP-Portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MyPortfolio');
+    expect(app.title).toEqual('AnthonyKP-Portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MyPortfolio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('AnthonyKP-Portfolio app is running!');
   });
 });
